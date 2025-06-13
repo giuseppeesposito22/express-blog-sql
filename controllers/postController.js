af50261aa992065ae8e8b58d3e8ffe45ed538622
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-13 16:23:32 +0200

    add destroy function with db

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,7 +16,7 @@ const index = (req, res) => {
   const sql = "SELECT * FROM posts";
 
   connection.query(sql, (err, results) => {
-    if (err) return res.status(500).json({ error: "Database quesry failed" });
+    if (err) return res.status(500).json({ error: "Database query failed" });
     console.log(results);
 
     res.json({
@@ -98,6 +98,22 @@ const destroy = (req, res) => {
   // posts.splice(post, 1);
   // console.log(posts);
   // res.status(204).json("No content");
+
+  const id = parseInt(req.params.id);
+
+  const sql = "DELETE FROM posts WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Database query failed" });
+    }
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.status(204);
+  });
 };
 
 module.exports = { index, show, store, update, destroy };
