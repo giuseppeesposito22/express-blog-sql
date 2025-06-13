3318d5f1c4f4818e4d69619114369942dafe9f85
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-13 15:25:58 +0200

    add db connection

--- data/db.js
+++ data/db.js
@@ -1,1 +1,16 @@
+const mySql = require("mysql2");
 
+const connection = mySql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "blog",
+  port: 3306,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = connection;
